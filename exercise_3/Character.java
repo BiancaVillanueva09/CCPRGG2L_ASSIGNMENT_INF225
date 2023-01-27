53a6e9005cd449e01eb6441477670191eedeae1b
Bianca Villanueva <111473235+BiancaVillanueva09@users.noreply.github.com>
2023-01-27 08:40:57 +0800

    Create Character.java

---
+++ exercise_3/Character.java
@@ -0,0 +1,10 @@
+public class Character {
+    String charactername;
+    String attack;
+    int additionalDamage;
+
+    public void attack() {
+        System.out.println(" ");
+        System.out.println("I am attacking!!!");
+    }
+}
