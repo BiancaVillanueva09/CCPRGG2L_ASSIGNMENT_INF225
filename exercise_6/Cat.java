4fe2ea10ff4127ef31f8d7fbe5d27617e4163792
Bianca Villanueva <111473235+BiancaVillanueva09@users.noreply.github.com>
2023-02-10 11:50:00 +0800

    Create Cat.java

---
+++ exercise_6/Cat.java
@@ -0,0 +1,9 @@
+public class Cat implements Animal{
+    public void walk() {
+        System.out.println("Millie is walking around the house.");
+    }
+
+    public void run() {
+        System.out.println("Millie is running towards my sister.");
+    }
+}
