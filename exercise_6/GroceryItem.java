73820f2f9bd5b5ef46c7000b63cf7fcf477256e3
Bianca Villanueva <111473235+BiancaVillanueva09@users.noreply.github.com>
2023-02-10 11:44:37 +0800

    Create GroceryItem.java

---
+++ exercise_6/GroceryItem.java
@@ -0,0 +1,12 @@
+public class GroceryItem {
+
+    String name;
+    public int price;
+
+    public void showItemName() {
+        System.out.println(this.name);
+    }
+
+    public void showItemprice() {
+    }
+}
