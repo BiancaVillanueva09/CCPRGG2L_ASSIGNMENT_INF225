fd594d0857d447586cad0e37e64593ba9c703d4c
Bianca Villanueva <111473235+BiancaVillanueva09@users.noreply.github.com>
2023-02-10 10:10:03 +0800

    Create Daddy.java

---
+++ exercise_5/Daddy.java
@@ -0,0 +1,8 @@
+public class Daddy {
+    protected String surname = "Villanueva";
+    
+    public void showLove() {
+        System.out.println("I love my " + surname + " family.");
+    }
+    
+}
